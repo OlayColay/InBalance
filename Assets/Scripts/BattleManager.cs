ca106de6814c7b50be453a243488172583a92579
Cole Strain <colestrain2000@gmail.com>
2021-10-01 23:27:56 -0700

    Start work on battle system!

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Player's turn, so open necessary UI and let player control in it
+    }
+}
