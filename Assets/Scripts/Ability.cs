7aec91ac8058bd245437a9012b19e0cdf119f7a4
Cole Strain <colestrain2000@gmail.com>
2021-10-01 22:29:35 -0700

    Make Ability have virtual variables

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -6,11 +6,11 @@ using static Constants;
 public class Ability : MonoBehaviour
 {
     /// <summary> Type of the ability that the receiving actor could be weak/strong against </summary>
-    public Type type;
+    public virtual Type type {get; set;}
 
     /// <summary> Base damage of ability that could be affected by user's and target's types and stats </summary>
-    public int power = 10;
+    public virtual int power {get; set;}
 
     /// <summary> What the name of the ability is on menus or info screens </summary>
-    public string displayName;
+    public virtual string displayName {get; set;}
 }
