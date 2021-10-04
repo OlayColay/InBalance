102e3387b3cd5e55a0d9d974b1ed7b54b9a8b490
Cole Strain <colestrain2000@gmail.com>
2021-10-04 14:06:10 -0700

    Music compatibility for overworld and battles

--- Assets/Scripts/Overworld/StartBattle.cs
+++ Assets/Scripts/Overworld/StartBattle.cs
@@ -8,6 +8,8 @@ public class StartBattle : MonoBehaviour
     /// <summary> The opponents that the player will fight in the battle </summary>
     public GameObject[] opponents;
 
+    public AudioSource music;
+
     private Collider2D player;
 
     private bool battledOnce = false;
@@ -17,6 +19,7 @@ public class StartBattle : MonoBehaviour
         Debug.Log("Start Battle");
         if (other.tag == "Player")
         {
+            music.Pause();
             GameObject.FindObjectOfType<OverworldMovement>().controls.Overworld.Disable();
             SceneManager.LoadScene("Battle", LoadSceneMode.Additive);
 
