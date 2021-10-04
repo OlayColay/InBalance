6c350ca6457b2b78572a8ba07f44e13b374ce8ea
AndrewYiBC <yiandrew2001@gmail.com>
2021-10-04 14:52:29 -0700

    Added initial cutscene

---
+++ Assets/Scripts/Cutscene/Cutscene_1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene_1 : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    private Controls controls;
+    private int pressCount; 
+
+    private void Awake()
+    {
+        pressCount = 0;
+        controls = new Controls();
+        controls.Overworld.Interact.performed += pressed =>
+        {
+            pressCount += 1;
+            switch (pressCount)
+            {
+                case 1:
+                    dialogueBox.SetActive(true);
+                    dialogueBox.GetComponent<RunMultipleDialogue>().loadDialogue(5, 1);
+                    dialogueBox.GetComponent<RunMultipleDialogue>().startDialogue();
+                    break;
+                case 2:
+                    dialogueBox.GetComponent<RunMultipleDialogue>().loadDialogue(6, 1);
+                    dialogueBox.GetComponent<RunMultipleDialogue>().startDialogue();
+                    break;
+                case 3:
+                    dialogueBox.GetComponent<RunMultipleDialogue>().loadDialogue(21, 1);
+                    dialogueBox.GetComponent<RunMultipleDialogue>().startDialogue();
+                    break;
+                default:
+                    SceneManager.LoadScene("0_House");
+                    break;
+            }
+        };
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
